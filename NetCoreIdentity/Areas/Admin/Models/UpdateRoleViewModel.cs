acc889c3802bfa02faac6b529c71d2cce36fdab2
ozgurgunay <gnyozgur@yandex.com>
2023-12-06 15:27:34 +0300

    role authorization.

---
+++ NetCoreIdentity/Areas/Admin/Models/UpdateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreIdentity.Areas.Admin.Models
+{
+    public class UpdateRoleViewModel
+    {
+        public string Id { get; set; } = null!;
+        [Required(ErrorMessage = "Role Name can not be left empty!")]
+        [Display(Name = "Role Name :")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ NetCoreIdentity/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using NetCoreIdentity.Models;
+using System.Text;
+
+namespace NetCoreIdentity.TagHelpers
+{
+    public class UserRoleNamesTagHelper : TagHelper
+    {
+        public string UserId { get; set; } = null!;
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await _userManager.FindByIdAsync(UserId);
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var stringBuilder = new StringBuilder();
+
+            userRoles.ToList().ForEach(x =>
+            {
+                //@ means -> If you put an @ sign in front of it, you can arrange the spans as you wish in terms of page layout and font style.
+                stringBuilder.Append(@$"<span class='badge bg-dark mx-1'>{x.ToLower()}</span>");
+            });
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+        }
+    }
+}
